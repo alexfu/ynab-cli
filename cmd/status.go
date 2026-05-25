13a1ee0a4a39ae975914b5c3740e1d9a35c6f939
Alex Fu <alexfu@fastmail.com>
2026-05-25 06:41:23 -0400

    add an auth status command

---
+++ cmd/status.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/zalando/go-keyring"
+)
+
+// statusCmd represents the status command
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Show status of auth.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		_, err := keyring.Get("ynab-cli", "default")
+		if err == nil {
+			fmt.Println("Logged in.")
+		} else {
+			if errors.Is(err, keyring.ErrNotFound) {
+				fmt.Println("Not logged in. Run `ynab login`.")
+			} else {
+				return fmt.Errorf("read token from keyring: %w", err)
+			}
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(statusCmd)
+}
